ab5874016d3e741f8fffc2b3e711082700b29ad1
Tapiens-Dev <ftapia@MacBook-Air-de-Felipe.local>
2024-07-02 22:36:10 -0400

    crear clean arquitecture, componente de los input, separar los estilos de las vistas

---
+++ src/Presentation/views/register/ViewModel.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+const RegisterViewModel = () => {
+
+
+    const [values, setValues] = useState({
+        name: '',
+        lastname: '',
+        email: '',
+        phone: '',
+        password: '',
+        confirmPassword: ''
+    });
+
+    const onChange = (property: string, value: any) => {
+
+        setValues({ ...values, [property]: value });
+    }
+
+    const register = () => {
+        console.log(JSON.stringify(values));
+    }
+
+  return  {
+    ...values,
+    onChange,
+    register
+  }
+
+ 
+}
+
+export default RegisterViewModel
